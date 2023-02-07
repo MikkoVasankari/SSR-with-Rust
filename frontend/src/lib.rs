19221fe14bb99e205a254f0dfb20a7e4bdcfe331
MikkoVasankari <75028888+MikkoVasankari@users.noreply.github.com>
2023-02-07 11:33:59 +0200

    Cleaning up

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -10,22 +10,28 @@ pub fn app() -> Html {
                 {"Blog post "}
             </h1>
 
-            <h3>
-                {"Author: "}
+            <h3 style="text-decoration: underline;">
+                {"Author: 1"}
             </h3>
-            <p> {"Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac
-            turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, 
-            tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.
-             Aenean ultricies mi vitae est. Mauris placerat eleifend leo. 
-             Quisque sit amet est et sapien ullamcorper pharetra. 
-             Vestibulum erat wisi, condimentum sed, commodo vitae, 
-             ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, 
-             eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. 
-             Donec non enim in turpis pulvinar facilisis. Ut felis. 
-             Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, 
-             eu vulputate magna eros eu erat. Aliquam erat volutpat. 
-             Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus"}
-             </p>
+            <p > {"Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Cras id mi a quam malesuada gravida. Morbi lorem libero, porta quis ultrices ac, 
+            molestie quis est. Donec fringilla, ipsum a consequat maximus, 
+            dui purus posuere tortor, et iaculis enim est at velit. Pellentesque 
+            habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+            Donec sit amet sem eleifend, tristique lorem et, vulputate eros. 
+            Proin ac viverra justo, ac mattis leo. Aliquam mollis erat id massa sodales, 
+            at venenatis velit varius. Cras sed molestie neque, non sodales massa. "}
+            </p>
+
+            <p> {"Mauris eleifend egestas finibus. Vestibulum hendrerit maximus turpis. 
+            Donec molestie, dui ullamcorper tempor tincidunt, arcu erat pulvinar massa, 
+            sit amet mattis dolor orci ac arcu. Suspendisse tempor lacus nisl, in feugiat arcu mollis sit amet.
+             Mauris tincidunt orci eros, a tempor urna elementum vitae. Curabitur augue sem, 
+             tempus at quam sit amet, rutrum porttitor risus. Vestibulum eget facilisis nibh, 
+             vel dapibus massa. Maecenas blandit, eros sit amet volutpat semper, 
+             lorem tellus ornare tellus, a vehicula lacus nulla id odio. Sed dapibus 
+             enim sit amet erat iaculis, at sollicitudin risus mattis. "}
+            </p>
 
         </div>
         </>
